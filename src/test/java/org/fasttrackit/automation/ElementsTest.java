a6b7f172f43151db45534b79c6a00934b0c51326
zoltankg <zoltankarczag77@gmail.com>
2017-02-22 13:28:45 +0200

    extract elements in class definition

--- src/test/java/org/fasttrackit/automation/ElementsTest.java
+++ src/test/java/org/fasttrackit/automation/ElementsTest.java
@@ -16,10 +16,11 @@ public class ElementsTest extends TestBase {
     private LoginView loginPgage = new LoginView();
     private ElementsView checkOnCheckbox = new ElementsView();
 
-    private CheckBox stopProcessCheckbox = new CheckBox().setElPath("/html/body/form[1]/div[3]/label/input");
-    private CheckBox labelWithEnterCheckbox = new CheckBox().setElPath("/html/body/form[1]/div[4]/label/input");
     private WebLocator stopProcessLabel = new WebLocator().setText("Stop the process?", SearchType.TRIM);
     private WebLocator withEnterLabel = new WebLocator().setText("Label with Enter.", SearchType.CHILD_NODE);
+    private CheckBox stopProcessCheckbox = new CheckBox().setElPath("/html/body/form[1]/div[3]/label/input");
+    private CheckBox labelWithEnterCheckbox = new CheckBox().setElPath("/html/body/form[1]/div[4]/label/input");
+
 
 
     @Test
