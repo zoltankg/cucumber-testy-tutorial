7f3bacc32340baadfc656b876ca62fc56e7d7a9d
zoltankg <zoltankarczag77@gmail.com>
2017-02-23 20:23:50 +0200

    use TableRows as container for TableCell

--- src/test/java/org/fasttrackit/automation/TablesTest.java
+++ src/test/java/org/fasttrackit/automation/TablesTest.java
@@ -2,6 +2,11 @@ package org.fasttrackit.automation;
 
 
 import com.sdl.selenium.web.WebLocator;
+import com.sdl.selenium.web.button.Button;
+import com.sdl.selenium.web.form.CheckBox;
+import com.sdl.selenium.web.table.Cell;
+import com.sdl.selenium.web.table.Row;
+import com.sdl.selenium.web.table.Table;
 import org.fasttrackit.util.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -9,7 +14,7 @@ import org.testng.annotations.Test;
 
 public class TablesTest extends TestBase {
 
-
+    Table table = new Table();
 
     @Test
     public void selectTableRowTest(){
@@ -18,6 +23,19 @@ public class TablesTest extends TestBase {
 
         //WebElement checkbox = driver.findElement(By.xpath("//table//input"));
         WebLocator checkbox = new WebLocator().setElPath("//table//input");
+
+        checkbox.click();
+    }
+
+    @Test
+    public void selectRowByEmail() {
+        openLoginPage();
+        loginView.login("eu@fast.com", "eu.pass");
+
+        Row row = table.getRow(new Cell(4, "davidmiller@mail.com")); //Source from GitHub/ Tasty/ Table and row examples
+
+        //WebLocator checkbox = new WebLocator(row).setTag("input"); The first solution with WebLocator!
+        CheckBox checkbox = new CheckBox(row);
 
         checkbox.click();
     }
